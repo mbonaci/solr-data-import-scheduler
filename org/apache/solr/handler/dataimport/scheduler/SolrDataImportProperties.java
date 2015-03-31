e8e70209756b22c5b243b7d2e3a572681c3dab0f
Marko Bonaci <mbonaci@gmail.com>
2015-03-31 02:19:03 +0200

    Fix indentation

--- org/apache/solr/handler/dataimport/scheduler/SolrDataImportProperties.java
+++ org/apache/solr/handler/dataimport/scheduler/SolrDataImportProperties.java
@@ -27,52 +27,51 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Used to load settings from global dataimport.properties<br>
- * Uses java.util.Properties to load settings from dataimport.properties
+ * Uses java.util.Properties to load scheduler configuration from dataimport.properties
  * @author mbonaci
  */
 public class SolrDataImportProperties {
-        private Properties properties;
+    private Properties properties;
 
-        public static final String SYNC_ENABLED         = "syncEnabled";
-        public static final String SYNC_CORES           = "syncCores";
-        public static final String SERVER               = "server";
-        public static final String PORT                 = "port";
-        public static final String WEBAPP               = "webapp";
-        public static final String PARAMS               = "params";
-        public static final String INTERVAL             = "interval";
+    public static final String SYNC_ENABLED = "syncEnabled";
+    public static final String SYNC_CORES   = "syncCores";
+    public static final String SERVER       = "server";
+    public static final String PORT         = "port";
+    public static final String WEBAPP       = "webapp";
+    public static final String PARAMS       = "params";
+    public static final String INTERVAL     = "interval";
 
-        private static final Logger logger = LoggerFactory.getLogger(SolrDataImportProperties.class);
+    private static final Logger logger = LoggerFactory.getLogger(SolrDataImportProperties.class);
 
-        public SolrDataImportProperties(){
-//              loadProperties(true);
+    public SolrDataImportProperties() {
+//          loadProperties(true);
+    }
+
+    public void loadProperties(boolean force) {
+        try {
+            SolrResourceLoader loader = new SolrResourceLoader(null);
+            logger.info("Instance dir = " + loader.getInstanceDir());
+
+            String configDir = loader.getConfigDir();
+            configDir = SolrResourceLoader.normalizeDir(configDir);
+            if (force || properties == null) {
+                properties = new Properties();
+
+                File dataImportProperties = new File(configDir, "dataimport.properties");
+
+                FileInputStream fis = new FileInputStream(dataImportProperties);
+                properties.load(fis);
+            }
+        } catch(FileNotFoundException fnfe) {
+            logger.error("Error locating DataImportScheduler dataimport.properties file", fnfe);
+        } catch(IOException ioe) {
+            logger.error("Error reading DataImportScheduler dataimport.properties file", ioe);
+        } catch(Exception e) {
+            logger.error("Error loading DataImportScheduler properties", e);
         }
+    }
 
-        public void loadProperties(boolean force){
-                try{
-                        SolrResourceLoader loader = new SolrResourceLoader(null);
-                        logger.info("Instance dir = " + loader.getInstanceDir());
-
-                        String configDir = loader.getConfigDir();
-                        configDir = SolrResourceLoader.normalizeDir(configDir);
-                        if(force || properties == null){
-                                properties = new Properties();
-
-                                File dataImportProperties = new File(configDir, "dataimport.properties");
-
-                                FileInputStream fis = new FileInputStream(dataImportProperties);
-                                properties.load(fis);
-                        }
-                }catch(FileNotFoundException fnfe){
-                        logger.error("Error locating DataImportScheduler dataimport.properties file", fnfe);
-                }catch(IOException ioe){
-                        logger.error("Error reading DataImportScheduler dataimport.properties file", ioe);
-                }catch(Exception e){
-                        logger.error("Error loading DataImportScheduler properties", e);
-                }
-        }
-
-        public String getProperty(String key){
-                return properties.getProperty(key);
-        }
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
